3f9d9972ed564b443c2ccd1f0bec746a1696894e
Joel Pineda <joel.pineda@unitec.edu>
2014-06-09 00:28:30 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Projectmongodb::Application.config.secret_key_base = '90838b36141638fc12ba4ce2d00f8db1b81090faba96c4afc8a9e51f4ec1a48724ccb6fd6824a5d468bad07ebf394dd54f092986bfb780f79ec1cb012be6dc8c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Projectmongodb::Application.config.session_store :cookie_store, key: '_Projectmongodb_session'
